ded35ce3f73a851c55373f6fe149d5af90d9f365
A01177145 <A01177145@itesm.mx>
2022-05-23 23:47:24 -0500

    Ventas mejorado

--- index.php
+++ index.php
@@ -32,36 +32,63 @@
     </div>
   </head>
   <body>
-      <!--- Creamos una tabla con bootstrap para registrar las ventas -->
+      <!--- Crear una tabla horizontal con bootstrap para registrar las ventas -->
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h3>Registrar Venta</h3>
-                        </div>
-                        <div class="card-body">
-                            <form action="send.php" method="POST">
-                                <div class="form-group">
-                                    <label for="fruta">Fruta</label>
-                                    <input type="text" class="form-control" id="fruta" name="fruta" placeholder="Fruta" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="cantidad">Cantidad</label>
-                                    <input type="number" class="form-control" id="cantidad" name="cantidad" placeholder="Cantidad" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="sucursal">Sucursal</label>
-                                    <input type="text" class="form-control" id="sucursal" name="sucursal" placeholder="Sucursal" required>
-                                </div>
-                                <div class="form-group">
-                                    <label for="precio">Precio</label>
-                                    <input type="number" class="form-control" id="precio" name="precio" placeholder="Precio" required>
-                                </div>
-                                <button type="submit" class="btn btn-primary">Enviar</button>
-                            </form>
-                        </div>
-                    </div>
+                    <h1 class="display-1 text-center">Sistema de ventas</h1>
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Fruta</th>
+                                <th>Cantidad</th>
+                                <th>Precio</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <!---Usuario ingresa productos dentro de la tabla-->
+                            <tr>
+                                <form action="send.php" method="POST">
+                                    <td><select class="form-control" name="fruta" id="fruta" required>
+                                        <option value="">Seleccione una fruta</option>
+                                        <option value="Fresa">Fresa</option>
+                                        <option value="Mango">Mango</option>
+                                    </select></td>
+                                    <td><input type="text" name="cantidad" class="form-control" id="cantidad" placeholder="Cantidad" required></td>
+                                    <td><input type="text" name="precio" class="form-control" id="precio" placeholder="Precio" required></td>
+                                    <td><input type="submit" class="btn btn-primary" value="Enviar"></td>
+                                </form>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <!---Desplegamos los ultimos 10 productos entregados-->
+                    <h3 class="display-6 text-center pd-10">Ultimas entregas</h3>
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Fruta</th>
+                                <th>Precio</th>
+                                <th>Fecha</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                                //Conectamos a la base de datos
+                                require_once 'connect.php';
+                                //Seleccionamos los ultimos 10 productos entregados
+                                $sql = "SELECT * FROM ventas ORDER BY id DESC LIMIT 10";
+                                $result = mysqli_query($link, $sql);
+                                //Creamos una tabla con los datos de la base de datos
+                                while($row = mysqli_fetch_array($result)){
+                                    echo "<tr>";
+                                    echo "<td>" . $row['Fruta'] . "</td>";
+                                    echo "<td>" . $row['Precio'] . "</td>";
+                                    echo "<td>" . $row['Fecha'] . "</td>";
+                                    echo "</tr>";
+                                }
+                            ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
--- send.php
+++ send.php
@@ -6,20 +6,28 @@ $cantidad = $_REQUEST['cantidad'];
 $sucursal = $_REQUEST['sucursal'];
 $precio = $_REQUEST['precio'];
 
-while($cantidad >= 1) {
-  $sql = "INSERT INTO ventas (Fruta, Precio, Sucursal) VALUES ";
-  $sql .= "('" . $fruta . "',";
-  $sql .= "'" . $precio . "',";
-  $sql .= "'" .$sucursal . "')";
-  $cantidad--;
 
-//print $sql;
-  if(mysqli_query($link, $sql)){
-    print ("Stored");
-  } else {
-    print("Failed");
-  }
+//Convertir el valor numerico de la fruta a uno legible.
+if($fruta == 1){
+    $fruta = "Fresa";
+}else if($fruta == 2){
+    $fruta = "Mango";
 }
+
+  while($cantidad >= 1) {
+    $sql = "INSERT INTO ventas (Fruta, Precio, Sucursal) VALUES ";
+    $sql .= "('" . $fruta . "',";
+    $sql .= "'" . $precio . "',";
+    $sql .= "'" .$sucursal . "')";
+    $cantidad--;
+
+  //print $sql;
+    if(mysqli_query($link, $sql)){
+      print ("Stored");
+    } else {
+      print("Failed");
+    }
+  }
 
 //regresar a index.php
 header("Location: index.php");
